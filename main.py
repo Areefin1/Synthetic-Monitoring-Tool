7083982c864ad3649a6d22699721cdffd411658c
MA <faisal.areefin97@qmail.cuny.edu>
2024-08-08 20:50:36 -0400

    Main Python file

---
+++ main.py
@@ -0,0 +1,9 @@
+from pingparser import main as launch_pingparser
+
+# Main function that only currently executes pingparser.py file. Will continue to add more functionality.
+def main():
+
+    launch_pingparser()
+
+if __name__ == "__main__":
+    main()

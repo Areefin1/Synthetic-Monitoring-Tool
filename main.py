2e6abc694a34df8579f0f6d17584df3d40c7af88
MA <faisal.areefin97@qmail.cuny.edu>
2024-08-23 17:06:48 -0400

    Added functionality of starting the Prometheus server

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from pingparser import main as launch_pingparser
+import subprocess
+import time
 
 # Main function that only currently executes pingparser.py file. Will continue to add more functionality.
 def main():
 
+    # Start the Prometheus server on port 9090
+    start_prometheus = ['./prometheusfolder/prometheus', '--config.file=./prometheusfolder/prometheus.yml']
+    subprocess.Popen(start_prometheus)
+
+    time.sleep(7)
+
     launch_pingparser()
+
+    print("Automatically shutting down Prometheus server on port 9090.")
 
 if __name__ == "__main__":
     main()
